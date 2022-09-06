ae0903409e9bfcb0b405499065635c434dbced7c
FrancoL <francovalentin5005@gmail.com>
2022-09-05 21:03:16 -0300

    Primera clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SynopsisComponent } from './synopsis/synopsis.component';
+import { CharactersComponent } from './characters/characters.component';
+import { MonstersComponent } from './monsters/monsters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SynopsisComponent,
+    CharactersComponent,
+    MonstersComponent
   ],
   imports: [
     BrowserModule
